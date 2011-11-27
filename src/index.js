816d14bef8fd40d0b50eaa7f835e23dfbf73e663
Charles Davison <charlie@powmedia.co.uk>
2011-11-27 16:55:31 +0000

    Use mongodb-native

---
+++ src/index.js
@@ -0,0 +1,154 @@
+//Dependencies
+var fs       = require('fs'),
+    mongo    = require('mongodb'),
+    ObjectID = require('mongodb/lib/mongodb/bson/bson').ObjectID,
+	async	 = require('async'),
+	_		 = require('underscore');
+
+
+var noop = function() {};
+
+
+/**
+ * Main method for connecting to the database and returning the fixture loader (Loader)
+ * 
+ * @param {String}  Database name
+ * @param {Object}  Connection options: host ('localhost'), port (27017)
+ */
+module.exports.connect = function(dbName, options) {
+    return new Loader(dbName, options);
+}
+
+
+
+/**
+ * Helper function that creates a MongoDB ObjectID given a hex string
+ * @param {String}  Optional hard-coded Object ID as string
+ */
+module.exports.createObjectId = function(str) {
+    if (str)
+        return new ObjectID(str);
+    else
+        return new ObjectID();
+};
+
+
+
+/**
+ * Loader constructor
+ * 
+ * @param {String}  Database name
+ * @param {Object}  Connection options: host ('localhost'), port (27017)
+ */
+var Loader = function(dbName, options) {
+    options = options || {};
+    
+    var host = options.host || 'localhost',
+        port = options.port || 27017;
+    
+    //Reference to collections that have been emptied; to prevent deleting new data when loading from directory
+    this.clearedCollections = [];
+    
+    //Connect
+    this.db = new mongo.Db(dbName, new mongo.Server(host, port, {}));
+};
+
+
+/**
+ * Clears a collection and inserts the given data as new documents
+ *
+ * @param {Mixed}       The data to load. This parameter accepts either:
+ *                          String: Path to a file or directory to load
+ *                          Object: Object literal in the form described in docs
+ * @param {Function}    Callback(err)
+ */
+Loader.prototype.load = function(data, cb) {
+	var self = this;
+	
+	var collectionNames = Object.keys(data);
+	
+	self.db.open(function (err, db) {
+		if (err) return cb(err);
+		
+		async.forEach(collectionNames, function(collectionName, next) {
+			var collectionData = data[collectionName];
+			
+			//Convert object to array
+			var items;
+			if (Array.isArray(collectionData)) {
+				items = collectionData.slice();
+			} else {
+				items = _.map(collectionData, function(item) {
+					return item;
+				});
+			}
+			
+			db.collection(collectionName, function(err, collection) {
+				if (err) return next(err);
+
+				collection.insert(items, next);
+			});
+		}, cb);
+	});
+};
+
+
+/**
+ * Loads fixtures from one file
+ * 
+ * @param {String}      The full path to the file to load
+ * @param {Function}    Optional callback(err)
+ */
+Loader.prototype.loadFile = function(file, callback) { 
+    callback = callback || function() {};
+    
+    if (file.substr(0, 1) !== '/') {
+        var parentPath = module.parent.filename.split('/');
+        parentPath.pop();
+        file = parentPath.join('/') + '/' + file;
+    }
+    
+    this.load(require(file), callback);
+}
+
+
+/**
+ * Loads fixtures from all files in a directory
+ * 
+ * TODO: Add callback option
+ * 
+ * @param {String}      The directory path to load e.g. 'data/fixtures' or '../data'
+ * @param {Function}    Optional callback(err)
+ */
+Loader.prototype.loadDir = function(dir, callback) {
+    callback = callback || function() {};
+    
+    var self = this;
+    
+    //Get the absolute dir path if a relative path was given
+    if (dir.substr(0, 1) !== '/') {
+        var parentPath = module.parent.filename.split('/');
+        parentPath.pop();
+        dir = parentPath.join('/') + '/' + dir;
+    }
+    
+    //Counters for managing callbacks
+    var total = 0,
+        done = 0;
+    
+    //Load each file in directory
+    fs.readdir(dir, function(err, files){
+        if (err) return callback(err);
+        
+        total = files.length;
+        
+        files.forEach(function(file) {
+            self.loadFile(dir + '/' + file, function(err) {
+                if (err) return callback(err);
+                
+                done++;
+                if (total === done) callback();
+            });
+        });
+    });
+};
---
+++ test/index.test.js
@@ -0,0 +1,169 @@
+//Nodeunit tests
+
+var fixtures = require('../src/index.js'),
+	id = fixtures.createObjectId,
+	mongo = require('mongodb'),
+	async = require('async'),
+	_ = require('underscore');
+
+var dbName = 'pow-mongodb-fixtures-test',
+	loader = fixtures.connect(dbName),
+	server = new mongo.Db(dbName, new mongo.Server('127.0.0.1', 27017, {})),
+	db;
+	
+	
+function loadCollection(name, cb) {
+	db.collection(name, function(err, collection) {
+		if (err) return cb(err);
+		
+		collection.find(function(err, cursor) {
+			if (err) return cb(err);
+			
+			cursor.toArray(function(err, docs) {
+				if (err) return cb(err);
+				
+				cb(null, docs);
+			});
+		});
+	});
+}
+
+exports['init'] = function(test) {
+	server.open(function(err, openDb) {
+		if (err) return test.done(err);
+		
+		db = openDb;
+		
+		db.dropDatabase(test.done);
+	});
+};
+	
+	
+exports['createObjectId'] = {
+	'with ID': function(test) {
+		var objId = id('4eca80fae4af59f55d000020');
+		
+		test.same(typeof objId, 'object');
+		test.same(objId.constructor.name, 'ObjectID');
+		test.same(objId.toString(), '4eca80fae4af59f55d000020');
+		
+		test.done();
+	},
+	
+	'without ID': function(test) {
+		var objId = id();
+		
+		test.same(typeof objId, 'object');
+		test.same(objId.constructor.name, 'ObjectID');
+		test.same(objId.toString().length, 24);
+		
+		test.done();
+	}
+};
+
+exports['load'] = {
+	setUp: function(done) {
+		db.dropDatabase(done);
+	},
+	
+	'array': function(test) {
+		test.expect(2);
+		
+		var data = {
+			southPark: [
+				{ name: 'Eric' },
+				{ name: 'Butters' },
+				{ name: 'Kenny' }
+			],
+			boredToDeath: [
+				{ name: 'Jonathan' },
+				{ name: 'Ray' },
+				{ name: 'George' }
+			]
+		};
+		
+		loader.load(data, function(err) {
+			if (err) return test.done(err);
+			
+			async.parallel([
+				function(next) {
+					loadCollection('southPark', function(err, docs) {
+						if (err) return next(err);
+						
+						var names = _.pluck(docs, 'name');
+						
+						test.same(names.sort(), ['Eric', 'Butters', 'Kenny'].sort());
+						
+						next();
+					});
+				},
+				function(next) {
+					loadCollection('boredToDeath', function(err, docs) {
+						if (err) return next(err);
+						
+						var names = _.pluck(docs, 'name');
+						
+						test.same(names.sort(), ['Jonathan', 'Ray', 'George'].sort());
+
+						next();
+					});
+				}
+			], test.done);
+		});
+	},
+	
+	'object': function(test) {
+		test.expect(2);
+		
+		var data = {
+			southPark: {
+				eric: { name: 'Eric' },
+				butters: { name: 'Butters' },
+				kenny: { name: 'Kenny' }
+			},
+			boredToDeath: {
+				jonathan: { name: 'Jonathan' },
+				ray: { name: 'Ray' },
+				george: { name: 'George' }
+			}
+		};
+		
+		loader.load(data, function(err) {
+			if (err) return test.done(err);
+
+			async.parallel([
+				function(next) {
+					loadCollection('southPark', function(err, docs) {
+						if (err) return next(err);
+						
+						var names = _.pluck(docs, 'name');
+						
+						test.same(names.sort(), ['Eric', 'Butters', 'Kenny'].sort());
+						
+						next();
+					});
+				},
+				function(next) {
+					loadCollection('boredToDeath', function(err, docs) {
+						if (err) return next(err);
+						
+						var names = _.pluck(docs, 'name');
+						
+						test.same(names.sort(), ['Jonathan', 'Ray', 'George'].sort());
+
+						next();
+					});
+				}
+			], test.done);
+		});
+	}
+};
+
+//Close DB connection and end process when done
+exports['exit'] = function(test) {
+	db.close();
+	
+	test.done();
+	
+	process.nextTick(process.exit);
+}
